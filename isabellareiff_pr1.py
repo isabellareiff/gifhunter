8a186162c9c4631863d1738e854ca7d9d83d1abf
Izzy <izzyreiff@gmail.com>
2021-02-23 00:20:15 -0500

    Update isabellareiff_pr1.py

--- isabellareiff_pr1.py
+++ isabellareiff_pr1.py
@@ -2,91 +2,69 @@ import socket
 import os
 from datetime import datetime
 
+# I had trouble with getting the date to work so I searched online about the datetime module, and came across these links which helped me realize I had to use "from datetime import datetime": https://stackoverflow.com/questions/42771909/difference-between-import-datetime-and-from-datetime-import-datetime-which, https://www.codecademy.com/forum_questions/555ec051937676658b0007d2
 
-#Whitman, Luke. CDT 22' A4. Assistance given to the author. 
-#Cadet Whitman let me see his code for project 2 and 
-#I found it useful in setting up the socket addressing.
-#He also gave me some tips in formatting my headers
-#So that everything properly displayed. West Point NY.
+#Thayer, Liam. CDT 22' A4. Assistance given to the author, Verbal Discussion. I was having serious issues with getting my virtual machine to work, and CDT Thayer helped me understand which firewall I had to take down in order for it to work, because I was focused on the wrong thing. CDT Thayer helped explain proper usage of the socket module, specifically socket addressing to me, as I was having a  lot of trouble with it. CDT Thayer also helped me understand that the header encoding had to be UTF-8, which although I knew, I didn't realize had to be explicitly stated. 21 February 2021. West Point, NY.
 
+#Jones, Colin. CDT 22' D2. Assistance given to the author, Teams Chat. CDT Jones helped me with some of the formatting of my responseCode headers as I was having trouble getting the exact foramtting text correct. 21 February 2021.West Point, NY.
 
-def createServer():
-	serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	serversocket.bind(('isabella.reiff.cs484.eecs.net',80))
+# https://emalsha.wordpress.com/2016/11/22/how-create-http-server-using-python-socket/ and https://emalsha.wordpress.com/2016/11/24/how-create-http-server-using-python-socket-part-ii/. These websites were helpful for knowing which specific methods to use when I got stuck trying to figure things out, like AFINET and SOCK_STREAM.
+def makeServ():
+	realsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	realsocket.bind(('isabella.reiff.cs484.eecs.net',80))
+	realsocket.listen(5)
+
 	print(socket.gethostname())
-	serversocket.listen(5)
 
-
-	rightnow = datetime.now()
-	dtg = rightnow.strftime("%m/%d/%Y, %H:%M:%S")
-
-	#<304 stuff>
-	with open('moved') as f:
-		content = f.readlines()
-	content = [x.strip() for x in content]
-	mydict = {}
-	for i in range(len(content)):
-		content[i] = content[i].split(' ')
-
-	for elem in content:
-		mydict[elem[0]] = elem[1]
-	#</304 stuff>
-
+	time = datetime.now()
+	fulltime = time.strftime("%m/%d/%Y, %H:%M:%S")
+	with open('moved') as file:
+		fileRead = file.readlines()
+	fileRead = [type.strip() for type in fileRead]
+	book = {}
+	for i in range(len(fileRead)):
+		fileRead[i] = fileRead[i].split(' ')
+	for j in fileRead:
+		book[j[0]] = j[1]
 
 	while(1):
-		clientsocket, address = serversocket.accept()
-		req = clientsocket.recv(1024).decode('utf-8')
-		print(req)
-		stringlist = req.split(' ')
-		method = stringlist[0]
-		requesting = stringlist[1]
-		
-		print('client request \n', requesting)
-		
-		toserv = requesting.split('?')[0]
-		toserv = toserv.lstrip('/')
+		client, address = realsocket.accept()
+		request = client.recv(1024).decode('utf-8')
+		print(request)
+		strsplit = request.split(' ')
+		em = strsplit[0]
+		streReq = strsplit[1]
+		serve = streReq.split('?')[0]
+		serve = serve.lstrip('/')
 		header = 'HTTP/1.1 200 OK\r\n'
-		if(toserv == ''):
-			toserv = 'test1.html'
-		elif toserv in mydict.keys():
-			print('\n\n', toserv, '\n\n')
-			toserv = mydict[toserv]
-			print('\n\n', toserv, '\n\n')
-			header = 'HTTP/1.1 301 Moved\r\nLocation: http://isabella.reiff.cs484.eecs.net/' + toserv + '\r\n'
-
+		if(serve == ''):
+			serve = 'test1.html'
+		elif serve in book.keys():
+			print('\n\n', serve, '\n\n')
+			serve = book[serve]
+			print('\n\n', serve, '\n\n')
+			header = 'HTTP/1.1 301 Moved\r\nLocation: http://isabella.reiff.cs484.eecs.net/'+serve+'\r\n'
 		try:
-			file = open(toserv, 'rb')
-			response = file.read()
-			bytelength = os.path.getsize(toserv)
-			if(toserv.endswith('.jpg')):
-				mime = 'image/jpg'
-			elif(toserv.endswith('.mp4')):
-				mime = 'video/mp4'
+			file = open(serve, 'rb')
+			responseCode = file.read()
+			bytes = os.path.getsize(serve)
+			if(serve.endswith('.mp4')):
+				ft = 'video/mp4'
+			elif(serve.endswith('.jpg')):
+				ft = 'image/jpg'
 			else:
-				mime = 'text/html'				
-			
-			header += 'Last-Modified: Sun, 21 Feb 2021 23:59:59 GMT \n'
-			header += 'Date: '+ dtg +'\n'
+				ft = 'text/html'
+			header += 'Last-Modified: Mon, 22 Feb 2021 23:59:59 GMT \n'
+			header += 'Date: '+ fulltime +'\n'
 			header += 'Server: Custom\n'
-			header += 'Content-Length: ' + str(bytelength) + '\n'
-			header += 'Content-Type: ' + mime +'\n'
-			header += 'Connection: close\n'
-			header += '\n'
-
-		
+			header += 'Content-Length: ' + str(bytes) + '\n'
+			header += 'Content-Type: ' + ft +'\n'
+			header += 'Connection: close\n\n'
 		except Exception as e:
-			print(" \n we got a 404 \n")
 			header = 'HTTP/1.1 404 Not Found \n\n'
-			response = '<html><body><center><h3>Error 404: File Not Found</h3></center></body></html>'.encode()
-		print('THIS IS THE HEADER /n' ,header)
-		final_response = header.encode('utf-8')
-		final_response += response
-		clientsocket.send(final_response)
-		
-			
-
-
-		clientsocket.close()
-		
-
-createServer()
+			responseCode = '<html><body><center><h3>Error 404: File Not Found</h3></center></body></html>'.encode()
+		rC = header.encode('utf-8')
+		rC += responseCode
+		client.send(rC)
+		client.close()
+makeServ()
